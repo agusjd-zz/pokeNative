bd37bd5199b4a4c314d2523a362cd0d538b7eb9e
Agustin Diaz <agusjd92@gmail.com>
2022-01-06 13:39:23 -0300

    pokemon stats added

--- src/Screens/Pokemon.js
+++ src/Screens/Pokemon.js
@@ -3,12 +3,24 @@ import { View, Text } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import { getPokemonDetailsApi } from '../Api/Pokemon'
 import Header from '../Components/Pokemon/Header'
-import Type
- from '../Components/Pokemon/Type'
+import Type from '../Components/Pokemon/Type'
+import  Icon  from 'react-native-vector-icons/FontAwesome5'
+
+import Stats from '../Components/Pokemon/Stats'
 export default function Pokemon(props) {
     const {navigation,route:{params},} = props
 
     const [pokemon,setPokemon] = useState(null)
+
+
+    useEffect(()=>{
+        navigation.setOptions({
+            headerRight:()=> null,
+            headerLeft:() => <Icon name="arrow-left" color="red" size={20} style ={{
+                marginLeft:20
+            }} onPress={navigation.goBack} />
+        })
+    },[navigation,params])
 
     useEffect(()=>{
         (async()=>{
@@ -35,6 +47,7 @@ export default function Pokemon(props) {
                 type={pokemon.types[0].type.name} />
 
             <Type types={pokemon.types} />
+            <Stats stats={pokemon.stats} />
             </ScrollView>
             
         </View>
